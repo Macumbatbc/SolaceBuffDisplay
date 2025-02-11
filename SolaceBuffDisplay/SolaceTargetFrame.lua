febe4106c294f100f3ef351481a058c8d8a7363a
Macumba <85767653+Macumbatbc@users.noreply.github.com>
2025-02-11 14:22:40 +0100

    Add files via upload

--- SolaceBuffDisplay/SolaceTargetFrame.lua
+++ SolaceBuffDisplay/SolaceTargetFrame.lua
@@ -10,7 +10,16 @@ SolaceTargetFrameOptions = {
 
 function SolaceTargetFrame:onEvent(event, ...)
 	if ( ( event == "VARIABLES_LOADED" ) ) then
-
+		self["commandsInit"] = SolaceLib["commandsInit"]
+		self:commandsInit("showbuffs",SOLACE_SHOWBUFFS_HELP,"showbuffsHandler")
+		self:commandsInit("showdebuffs",SOLACE_SHOWDEBUFFS_HELP,"showdebuffsHandler")
+		self:commandsInit("showownbig",SOLACE_SHOWOWN_HELP,"showownHandler")
+		
+		-- DEFAULT_CHAT_FRAME:AddMessage(SOLACETARGETFRAME_WELCOME)
+		
+		self:showownHandler(SolaceTargetFrameOptions.showownbig,SolaceLib:getCmdindex("showownbig"))
+		self:showbuffsHandler(SolaceTargetFrameOptions.maxbuffs,SolaceLib:getCmdindex("showbuffs"))
+		self:showdebuffsHandler(SolaceTargetFrameOptions.maxdebuffs,SolaceLib:getCmdindex("showdebuffs"))
 	end
 end
 
@@ -28,11 +37,11 @@ function SolaceTargetFrame:showbuffsHandler(msg,index)
 		end
 		MAX_TARGET_BUFFS = msg
 		SolaceTargetFrameOptions.maxbuffs = msg
-		DEFAULT_CHAT_FRAME:AddMessage(SOLACE_SHOWBUFFS..MAX_TARGET_BUFFS)
+		-- DEFAULT_CHAT_FRAME:AddMessage(SOLACE_SHOWBUFFS..MAX_TARGET_BUFFS)
 		TargetFrame_UpdateAuras(_G["TargetFrame"])
 	else
-		DEFAULT_CHAT_FRAME:AddMessage("/solace "..SolaceLib.commands[index].cmd..": "..SolaceLib.commands[index].desc)
-		DEFAULT_CHAT_FRAME:AddMessage(SOLACE_SHOWBUFFS..MAX_TARGET_BUFFS)
+		-- DEFAULT_CHAT_FRAME:AddMessage("/solace "..SolaceLib.commands[index].cmd..": "..SolaceLib.commands[index].desc)
+		-- DEFAULT_CHAT_FRAME:AddMessage(SOLACE_SHOWBUFFS..MAX_TARGET_BUFFS)
 	end
 end
 
@@ -50,11 +59,11 @@ function SolaceTargetFrame:showdebuffsHandler(msg,index)
 		end
 		MAX_TARGET_DEBUFFS = msg
 		SolaceTargetFrameOptions.maxdebuffs = msg
-		DEFAULT_CHAT_FRAME:AddMessage(SOLACE_SHOWDEBUFFS..MAX_TARGET_DEBUFFS)
+		--- DEFAULT_CHAT_FRAME:AddMessage(SOLACE_SHOWDEBUFFS..MAX_TARGET_DEBUFFS)
 		TargetFrame_UpdateAuras(_G["TargetFrame"])
 	else
-		DEFAULT_CHAT_FRAME:AddMessage("/solace "..SolaceLib.commands[index].cmd..": "..SolaceLib.commands[index].desc)
-		DEFAULT_CHAT_FRAME:AddMessage(SOLACE_SHOWDEBUFFS..MAX_TARGET_DEBUFFS)
+		-- DEFAULT_CHAT_FRAME:AddMessage("/solace "..SolaceLib.commands[index].cmd..": "..SolaceLib.commands[index].desc)
+		-- DEFAULT_CHAT_FRAME:AddMessage(SOLACE_SHOWDEBUFFS..MAX_TARGET_DEBUFFS)
 	end
 end
 
@@ -63,15 +72,14 @@ function SolaceTargetFrame:showownHandler(msg,index)
 		LARGE_BUFF_SIZE = 21
 		LARGE_BUFF_FRAME_SIZE = 23
 		SolaceTargetFrameOptions.showownbig = msg
-		DEFAULT_CHAT_FRAME:AddMessage(SOLACE_SHOWOWNON)
+		-- DEFAULT_CHAT_FRAME:AddMessage(SOLACE_SHOWOWNON)
 	elseif ( msg == "off" ) then
 		LARGE_BUFF_SIZE = 17
 		LARGE_BUFF_FRAME_SIZE = 19
 		SolaceTargetFrameOptions.showownbig = msg
-		DEFAULT_CHAT_FRAME:AddMessage(SOLACE_SHOWOWNOFF)
+		-- DEFAULT_CHAT_FRAME:AddMessage(SOLACE_SHOWOWNOFF)
 	else
-		DEFAULT_CHAT_FRAME:AddMessage("/solace "..SolaceLib.commands[index].cmd..": "..SolaceLib.commands[index].desc)
-		DEFAULT_CHAT_FRAME:AddMessage(SOLACE_SHOWOWNCURRENT..SolaceTargetFrameOptions.showownbig)
+		-- DEFAULT_CHAT_FRAME:AddMessage("/solace "..SolaceLib.commands[index].cmd..": "..SolaceLib.commands[index].desc)
+		-- DEFAULT_CHAT_FRAME:AddMessage(SOLACE_SHOWOWNCURRENT..SolaceTargetFrameOptions.showownbig)
 	end
 end
-
